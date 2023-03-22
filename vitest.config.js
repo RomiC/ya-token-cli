dbe3172ebfce5aa2214c7b7183cf931720737a62
Roman Charugin <roman.charugin@acronis.com>
2023-03-22 21:44:46 +0200

    Setup environment

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    clearMocks: true,
+    coverage: {
+      all: true,
+      enabled: true,
+      exclude: ['lib/constants.js'],
+      include: ['index.js', 'lib/*.js'],
+      reporter: ['lcov', 'text']
+    }
+  }
+});
